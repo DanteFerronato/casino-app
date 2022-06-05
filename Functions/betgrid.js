2c976d3f09bcb10dcfa7646431fcd3750f2d8f39
Dante <danteferronato@epet12smandes.edu.ar>
2022-06-05 16:38:27 -0300

    Added movable input

---
+++ Functions/betgrid.js
@@ -0,0 +1,60 @@
+main_grid = Array()
+
+betInput = document.getElementById("bet-input")
+function getBetInput() {
+    betInput = document.getElementById("bet-input")
+    return betInput
+}
+
+inputOpen = false
+
+for (let i=1; i<26; i++) {
+    main_grid.push(new Array())
+    for (let j=1; j<8; j++){
+        main_grid[i-1].push(document.getElementById("betgrid-"+i+"-"+j))
+    }
+}
+
+specialCells = new Array (
+    cell0 = document.getElementById("betgrid-0"),
+    cell00 = document.getElementById("betgrid-00"),
+    cell000 = document.getElementById("betgrid-0-00"),
+    cellLow = document.getElementById("betgrid-low"),
+    cellHigh = document.getElementById("betgrid-high"),
+    cellEven = document.getElementById("betgrid-even"),
+    cellOdd = document.getElementById("betgrid-odd"),
+    cellRed = document.getElementById("betgrid-red"),
+    cellBlack = document.getElementById("betgrid-black"),
+    cellDz1 = document.getElementById("betgrid-dz-1"),
+    cellDz2 = document.getElementById("betgrid-dz-2"),
+    cellDz3 = document.getElementById("betgrid-dz-3"),
+)
+
+function betInterface(cell) {
+    //cell.addEventListener("mouseover", () => {
+            
+    //})
+    cell.addEventListener("click", () => {
+        if (!inputOpen) {
+            cell.insertBefore(betInput, null)
+            getBetInput()
+            console.log(betInput.getAttribute("visibility"),"",inputOpen)
+            betInput.setAttribute("visibility", "visible")
+        } else {
+            cell.insertBefore(betInput, null)
+            getBetInput()
+            betInput.setAttribute("visibility", "hidden")
+        }
+        inputOpen = !inputOpen
+    })
+}
+
+for (let i=0; i<25; i++) {
+    for (let j=0; j<7; j++){
+        let cell = main_grid[i][j]
+        betInterface(cell)
+    }
+}
+specialCells.forEach(cell => {
+    betInterface(cell)
+});
