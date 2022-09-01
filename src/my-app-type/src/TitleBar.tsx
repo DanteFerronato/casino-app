374f9ce529ddcdcc3d369ded0811dcfcc7b0b5df
Dante <danteferronato@epet12smandes.edu.ar>
2022-09-01 14:40:48 -0300

    Fixed betgrid

--- src/my-app-type/src/TitleBar.tsx
+++ src/my-app-type/src/TitleBar.tsx
@@ -14,17 +14,17 @@ function TitleBar() {
             <ul id={style["window-controls"]}>
                 <li id={style["minimise-btn"]}>
                     <svg viewBox="0 0 5 5" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0.5,2.5 h4 z" stroke="#cb2" stroke-width="1" />
+                        <path d="M0.5,2.5 h4 z" stroke="#cb2" strokeWidth="1" />
                     </svg>
                 </li>
                 <li id={style["window-btn"]}>
                     <svg viewBox="0 0 5 5" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1,1 h3v3h-3v-3 z" stroke="#cb2" stroke-width="1" fill="none" />
+                        <path d="M1,1 h3v3h-3v-3 z" stroke="#cb2" strokeWidth="1" fill="none" />
                     </svg>
                 </li>
                 <li id={style["close-btn"]}>
                     <svg viewBox="0 0 5 5" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1,1 L4,4 M4,1 L1,4 z" stroke="#cb2" stroke-width="1" />
+                        <path d="M1,1 L4,4 M4,1 L1,4 z" stroke="#cb2" strokeWidth="1" />
                     </svg>
                 </li>
             </ul>
